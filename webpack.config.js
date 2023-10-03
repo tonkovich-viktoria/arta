1374b4f3a204415b11c1518bb1eedf193ece8b81
Victoria Tonkovich <tonkovich.vika@gmail.com>
2023-10-03 15:30:23 +0300

    add plugin for copy files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -15,28 +16,36 @@ module.exports = {
 
   module: {
     rules: [
-        {
-            test: /\.html$/i,
-            loader: "html-loader",
-          },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(jpg|png|svg)$/,
-                type: 'asset/resource'
-            }
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        type: 'asset/resource'
+      }
     ],
   },
-  
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-             filename: 'style.css',
-           }),
+      filename: 'style.css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/i18n'),
+          to: path.resolve(__dirname, 'dist/i18n')
+        }
+      ]
+    })
   ],
 
   devServer: {
